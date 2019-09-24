51ddc442c4730f5220c7d28acf4166cf03124ea3
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-24 22:24:10 +0530

    Final

--- app/src/main/java/com/piyushmaheswari/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/retrofitdemo/MainActivity.java
@@ -1,11 +1,13 @@
 package com.piyushmaheswari.retrofitdemo;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -20,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout relativeLayout;
     private LinearLayout linearLayout;
+
+
+    private TextView d;
+    private Image i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
